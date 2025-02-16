377b771ce07a6771e576980fbd647da0bf4a053e
Mayank Nautiyal <mayanknautiyal0211@gmail.com>
2025-02-16 12:08:24 +0530

    about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId= 14453
+let accountEmail="mayabox@gmail.com"
+var accountPassword="12345"
+ accountCity="jaipur"//not a good way to do this but it works in js
+let accountState
+
+
+ //accountId=2 //not allowed
+ accountEmail="box@gmail.com"
+ accountPassword="238223"
+ accountCity="dehradun"
+
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+ //output
+//  │ (index) │ Values          │
+// ├─────────┼─────────────────┤
+// │ 0       │ 14453           │
+// │ 1       │ 'box@gmail.com' │
+// │ 2       │ '238223'        │
+// │ 3       │ 'dehradun'      │
+
+/* 
+ prefer not to use var
+ bcz of issue in block scope and functional scope
+*/
