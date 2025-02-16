db807625d435538fd033f4bb1c6ae69f540d6041
Mayank Nautiyal <mayanknautiyal0211@gmail.com>
2025-02-16 14:17:07 +0530

    moreOnOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -42,3 +42,47 @@ let someNum=40;
 let stringNum=String(someNum)
 console.log(stringNum) //40
 console.log(typeof stringNum);//string
+
+//!!!!!!!!!!!!!!!!!!!!!--operation!!!!!!!!!
+
+let value=3
+let negValue=-value
+console.log(negValue) //-3
+
+let str1="hello"
+let str2=" mayank"
+
+let str3= str1+str2
+console.log(str3) //hello mayank
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122 -agar string first hai toh sabko string mai treat kiya jayega
+console.log(1+2+"2")//32 -agar string last mai hai toh pehle jo conversion hai oh ho jayega
+
+//tricky conversion
+console.log(+true)//1
+console.log(+"")//0
+
+//increment oper
+let x = 5;
+
+console.log(++x); // 6 (First increases, then prints)
+console.log(x);   // 6 (Value remains updated)
+
+let y = 5;
+
+console.log(y++); // 5 (First prints, then increases)
+console.log(y);   // 6 (Value updated after execution)
+
+
+//decrement oper
+let a = 5;
+
+console.log(--a); // 4 (First decreases, then prints)
+console.log(a);   // 4 (Value remains updated)
+
+let b = 5;
+
+console.log(b--); // 5 (First prints, then decreases)
+console.log(b);   // 4 (Value updated after execution)
